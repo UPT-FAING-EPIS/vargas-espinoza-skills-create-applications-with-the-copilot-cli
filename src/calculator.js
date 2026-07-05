6f5eaae14a1494c9c79a3de93b42aac87e0c99f2
Jefferson Vargas <iovargasespinozajeff@gmail.com>
2026-07-05 11:31:17 -0500

    Complete exercise steps

---
+++ src/calculator.js
@@ -0,0 +1,40 @@
+function addition(a, b) {
+  return a + b;
+}
+
+function subtraction(a, b) {
+  return a - b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  if (b === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  return a / b;
+}
+
+function modulo(a, b) {
+  return a % b;
+}
+
+function power(a, b) {
+  return a ** b;
+}
+
+function squareRoot(a) {
+  return Math.sqrt(a);
+}
+
+module.exports = {
+  addition,
+  subtraction,
+  multiplication,
+  division,
+  modulo,
+  power,
+  squareRoot,
+};
